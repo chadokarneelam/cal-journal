4034c034fc504edbb547d79a5e3bbaa09263ce80
chadokarneelam <neelamchadokar21@gmail.com>
2022-06-25 00:34:36 +0530

    added a functional calendar 25/6

--- client/src/Journal.vue
+++ client/src/Journal.vue
@@ -2,19 +2,45 @@
   <div>
   <div class="row">
     <div class='col-md-2'></div>
-    <div class='col-md-8'>Calendar</div>
+    <div class='col-md-8 cal'>
+      <FullCalendar :options="calendarOptions" />
+    </div>
     <div class='col-md-2'></div>
   </div>
-    
+     
   </div>
 </template>
 <script>
+import '@fullcalendar/core/vdom' // solves problem with Vite
+import FullCalendar from '@fullcalendar/vue'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import interactionPlugin from '@fullcalendar/interaction'
+import listPlugin from '@fullcalendar/list'
+import timeGridPlugin from '@fullcalendar/timegrid'
+
 export default {
+  components: {
+    FullCalendar // make the <FullCalendar> tag available
+  },
   data() {
     return {
-      date:"date"
-    };
-  }
-};
+      calendarOptions: {
+        plugins: [ dayGridPlugin, interactionPlugin ,listPlugin,timeGridPlugin],
+        initialView: 'dayGridMonth',
+        dateClick: this.handleDateClick,
+        headerToolbar:{
+          left:'dayGridMonth,timeGridWeek,listWeek',
+          center:'title',
+          right:'prev today next'
+        }
+      }
+    }
+  },
+  methods: {
+    handleDateClick(){
+      alert('clicked')
+    }
+  },
+}
 </script>
 <style lang=""></style>
